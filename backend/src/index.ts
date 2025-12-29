e6ded38553f00ddf72adeeb698131b87b43e38fb
Navya Sinha <sinhanavya1875@gmail.com>
2025-12-29 12:51:58 +0530

    fix cors2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://code-editor-bhpj.vercel.app"],
+    origin: ["http://localhost:5173", "https://codeflow.navyasinha.xyz"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
